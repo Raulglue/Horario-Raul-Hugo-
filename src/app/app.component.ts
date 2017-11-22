882fb3a2a56b2808744b933c1fc2d61f9afbfea2
Raulglue <raul14-jl@hotmail.com>
2017-11-22 21:20:30 +0100

    menu completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { Nav } from 'ionic-angular/components/nav/nav';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
-
+  @ViewChild(Nav) nav: Nav;
+  rootPage: any = HomePage;
+  eso: boolean = false;
+  bach: boolean = false;
+  fp: boolean = false;
+  color: string = "primary";
+  esoprimero: boolean = false;
+  esosegundo: boolean = false;
+  esotercero: boolean = false;
+  bachprimero: boolean = false;
+  bachsegundo: boolean = false;
+  fpadmin: boolean = false;
+  fpdam: boolean = false;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -17,6 +29,51 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+  abrirEso() {
+    this.eso = !this.eso;
+  }
+  abrirBach() {
+    this.bach = !this.bach;
+  }
+  abrirFp() {
+    this.fp = !this.fp;
+  }
+  abrirHome() {
+
+  }
+  reset() {
+    this.eso = false;
+    this.bach = false;
+    this.fp = false;
+    this.esoprimero = false;
+    this.esosegundo = false;
+    this.esotercero = false;
+    this.bachprimero = false;
+    this.bachsegundo = false;
+    this.fpadmin = false;
+    this.fpdam = false;
+  }
+  abrirEsoPrimero() {
+    this.esoprimero = !this.esoprimero;
+  }
+  abrirEsoSegundo() {
+    this.esosegundo = !this.esosegundo;
+  }
+  abrirEsoTercero() {
+    this.esotercero = !this.esotercero;
+  }
+  abrirBachPrimero() {
+    this.bachprimero = !this.bachprimero;
+  }
+  abrirBachSegundo() {
+    this.bachsegundo = !this.bachsegundo;
+  }
+  abrirFpAdmin() {
+    this.fpadmin = !this.fpadmin;
+  }
+  abrirFpDam() {
+    this.fpdam = !this.fpdam;
   }
 }
 
